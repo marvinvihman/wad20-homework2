a514b7a6057e5eba5f204cd5d526e121c6b2ae8e
Indrek Anissimov <indrek.anissimov@gmail.com>
2020-10-25 19:20:46 +0200

    Javascript refactor

--- res/js/profiles.js
+++ res/js/profiles.js
@@ -1,31 +1,52 @@
 $(document).ready(function () {
-    $.getJSON("https://private-anon-afaad5f603-wad20postit.apiary-mock.com/profiles",
-        function (data) {
-            $.each(data, function (index, value){
-                let create = $('<div></div>',{
-                    class: 'user-profile'
-                })
-                create.appendTo('.main-container');
-                let image = $('<img />', {
-                    class: 'profile-avatar',
-                    src: value.avatar
-                });
-                image.appendTo(create);
-                let name = $('<h2></h2>', {
-                    class: "profile-name"
-                }).text(value.firstname + " " + value.lastname);
-                name.appendTo(create);
-                let wrapper = $('<div></div>', {
-                    class: "follow-button-wrapper"
-                });
-                wrapper.appendTo(create);
-                let follow = $('<button></button>', {
-                    class: "follow-button"
-                }).text("Follow");
-                follow.appendTo(wrapper);
-            });
-        });
+
+    fetchProfiles()
+        .then((data) => {
+            data.forEach(profile => {
+                let profileDiv = createProfileDiv(profile);
+                profileDiv.appendTo('.main-container');
+            })
+        })
+
     $(document).on("click", ".follow-button", function(){
         $(this).toggleClass(".follow-button following");
     })
 });
+
+
+function createProfileDiv(profile){
+    let create = $('<div></div>',{
+        class: 'user-profile'
+    })
+    let image = $('<img />', {
+        class: 'profile-avatar',
+        src: profile.avatar
+    });
+    image.appendTo(create);
+    let name = $('<h2></h2>', {
+        class: "profile-name"
+    }).text(profile.firstname + " " + profile.lastname);
+    name.appendTo(create);
+    let wrapper = $('<div></div>', {
+        class: "follow-button-wrapper"
+    });
+    wrapper.appendTo(create);
+    let follow = $('<button></button>', {
+        class: "follow-button"
+    }).text("Follow");
+    follow.appendTo(wrapper);
+
+    return create;
+}
+
+function fetchProfiles() {
+    return $.getJSON({
+            url: "https://private-anon-afaad5f603-wad20postit.apiary-mock.com/profiles",
+        success: function(response) {
+            return response;
+        },
+        error: function () {
+                console.log("Failed to aqcuire data.")
+        }
+    });
+}
